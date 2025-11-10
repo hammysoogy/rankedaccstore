e9ea0ba39eb0b830da06a6706e072824def5b418
hammysoogy <bellaozuba@gmail.com>
2025-11-10 16:01:19 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -217,7 +217,7 @@ async def update_embed(interaction: discord.Interaction, send_message: bool = Fa
             "**# PAYMENT METHODS**\n"
             f"**# STATUS: {bot.status_emoji}**\n"
             "> <:emoji:1415780267154477147>  **`400 Robux`**\n"
-            "> <:loscomb:1427626337140609034>  **`15M/s`**\n\n"
+            "> <:emoji:1437458717963583538>  **`7day Zenith or Wave keys`**\n\n"
             f"Check out <#{RULES_CHANNEL_ID}> to see that we’re legit!"
         ),
         color=discord.Color.from_str("#A2FA50"),
